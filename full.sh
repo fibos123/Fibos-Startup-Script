1289bbdbc1b7faf978b9e53b82243ea9a7f6d658
Mingfun Wong <mingfun.wong.chn@gmail.com>
2020-11-25 19:34:41 +0900

    Create full.sh

---
+++ full.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+sudo apt-get update
+sudo apt-get install cockpit -y
+sudo apt-get install cockpit-docker -y
+sudo apt-get install cockpit-pcp -y
+sudo apt-get install docker-compose -y
+sudo systemctl start cockpit
+sudo systemctl enable cockpit
+
+cd ~
+git clone https://github.com/fibos123/fibos-node.git
+cd ./fibos-node
+
+sed -i 's/your-account-name/这里替换成你的账号/' ./_config.js
+sed -i 's/your-public-key/这里替换成你的公钥/' ./_config.js
+sed -i 's/your-private-key/这里替换成你的私钥/' ./_config.js
+sed -i 's/"producer-enable": false/"producer-enable": true/' ./_config.js
+
+url=$(curl api.fibos123.com/last_ghost)
+wget -O data.tar.gz $url
+tar -zxvSf data.tar.gz
+rm data.tar.gz
+sudo docker-compose up -d
