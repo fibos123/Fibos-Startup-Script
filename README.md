df17de2848dc20914b334d35ba459cf525486d34
Mingfun Wong <mingfun.wong.chn@gmail.com>
2021-10-25 23:43:34 +0900

    add k8s

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ OS: Ubuntu 16.04 / 18.04 / 20.04
 
 ## Script
 
-[Snapshot](https://github.com/fibos123/Fibos-Startup-Script/blob/master/snapshot.sh) : Disk: 20G~
+[Snapshot](https://github.com/fibos123/Fibos-Startup-Script/blob/master/k8s-snapshot.sh) : Disk: 20G~
 
+<!--
 [Full](https://github.com/fibos123/Fibos-Startup-Script/blob/master/full.sh) : Disk: 80G~
---
+++ k8s-snapshot.sh
@@ -0,0 +1,27 @@
+NAME=fibos-node-1
+PRODUCER_ENABLE=false
+PRODUCER_NAME=fibos123comm
+PUBLIC_KEY=
+PRIVATE_KEY=
+SNAPSHOT_ENABLE=true
+SNAPSHOT_URL=https://github.com/andy-backup/fibos/raw/master/snapshot.bin
+HTTP_HOST=
+
+# install k3s (Lightweight Kubernetes)
+curl -sfL https://get.k3s.io | sh -
+# install helm
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+# setup helm
+mkdir ~/.kube
+sudo kubectl config view --raw >~/.kube/config
+# add helm repo
+helm repo add mingfunwong https://mingfunwong.github.io/helm-charts
+# install fibos-node
+helm install $NAME mingfunwong/fibos-node \
+  --set PRODUCER_ENABLE=$PRODUCER_ENABLE\
+  --set PRODUCER_NAME=$PRODUCER_NAME\
+  --set PUBLIC_KEY=$PUBLIC_KEY\
+  --set PRIVATE_KEY=$PRIVATE_KEY\
+  --set SNAPSHOT_ENABLE=$SNAPSHOT_ENABLE\
+  --set SNAPSHOT_URL=$SNAPSHOT_URL \
+  --set ingress.hosts{0}.host=$HTTP_HOST
